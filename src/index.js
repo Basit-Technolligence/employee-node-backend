969da6bc25eaea8a07c41c9259a8285dd8addeb0
Basit-Technolligence <pakkobra007@gmail.com>
2020-10-26 19:38:16 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const dptRouter = require("./router/department-router");
 
 const noticeRouter = require("./router/notice-router");
 const port = process.env.PORT || 8000;
-// require("./db/mongoose");
+require("./db/mongoose");
 require("dotenv").config();
 const cors = require("cors");
 
